fcc0caaeba1158a3c4132660fa6f353aef200f7f
Your Name <prashantlaldas@gmail.com>
2021-06-30 16:56:26 +0530

    Initial commit

---
+++ app/src/main/java/com/prashantlal/breadandbutter/ViewHolder/ProductViewHolder.java
@@ -0,0 +1,40 @@
+package com.prashantlal.breadandbutter.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.prashantlal.breadandbutter.Interface.ItemClickListner;
+import com.prashantlal.breadandbutter.R;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public TextView txtProductName, txtProductDescription, txtProductPrice;
+    public ImageView imageView;
+    public ItemClickListner listner;
+
+
+    public ProductViewHolder(View itemView)
+    {
+        super(itemView);
+
+
+        imageView = (ImageView) itemView.findViewById(R.id.product_image);
+        txtProductName = (TextView) itemView.findViewById(R.id.product_name);
+        txtProductDescription = (TextView) itemView.findViewById(R.id.product_description);
+        txtProductPrice = (TextView) itemView.findViewById(R.id.product_price);
+    }
+
+    public void setItemClickListner(ItemClickListner listner)
+    {
+        this.listner = listner;
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        listner.onClick(view, getAdapterPosition(), false);
+    }
+}
+
